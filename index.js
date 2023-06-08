4883bc5b0b1000c8d93a007cd21918329d8cebd1
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-08 06:08:14 +0200

    Converter

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*
-
-1 meter = 3.281 feet --- 1 feet = 0,0003048
-1 liter = 0.264 gallon ---  1 gallon = 3,78541
-1 kilogram = 2.204 pound --- 1 pound = 0.454
-toFixed(3)
-
-*/
-
 const convertBtn = document.getElementById("convert-btn");
 let inputText = document.getElementById("input-text");
 let meterFeet = document.getElementById("meter-feet");
@@ -15,19 +6,28 @@ let kiloPounds = document.getElementById("kilograms-pounds");
 
 convertBtn.addEventListener("click", () => {
   let enteredValue = parseFloat(inputText.value);
+  let kilogram = Number(enteredValue) * 2.204;
+  let liter = Number(enteredValue) * 0.264;
+  let meter = Number(enteredValue) * 3.281;
+  let feet = Number(enteredValue) * 0.0003048;
+  let gallon = Number(enteredValue) * 3.78541;
+  let pound = Number(enteredValue) * 0.454;
+
   console.log(inputText.value, enteredValue);
   if (enteredValue === "") {
     return;
   }
-  let kilogram = Number(enteredValue) * 2.204;
-  let liter = Number(enteredValue) * 0.264;
 
   console.log(inputText.value * 3.281);
   console.log(inputText.value * 0.264);
   console.log(kilogram.toFixed(3));
-  meterFeet.textContent = `REMPLACE ${kilogram.toFixed(3)} `;
-  literGallon.textContent = `REMPLACE ${liter.toFixed(3)} `;
+  meterFeet.textContent = `${enteredValue} meters = ${meter.toFixed(
+    3
+  )} feet | ${enteredValue} feet = ${feet.toFixed(3)} meters `;
+  literGallon.textContent = `${enteredValue} liters = ${liter.toFixed(
+    3
+  )} gallons | ${enteredValue} gallons = ${gallon.toFixed(3)} liters`;
   kiloPounds.textContent = ` ${enteredValue} kilos = ${kilogram.toFixed(
     3
-  )} pounds | ${enteredValue} pounds = ${kilogram.toFixed(3)} kilos`;
+  )} pounds | ${enteredValue} pounds = ${pound.toFixed(3)} kilos`;
 });
